1fdd311023bf2de0c917c76b0391fc94f36792a9
ismailkhan4 <islamians96@gmail.com>
2022-06-15 18:02:17 +0500

    Filebase routing, Server Side Rendering, Api routes, mongoDB

---
+++ nextjs-practice/components/meetups/MeetupDetail.js
@@ -0,0 +1,19 @@
+import classes from './MeetupDetail.module.css';
+const MeetupDetail = ({
+  image,
+  title,
+  detail,
+  address,
+  description,
+  ...props
+}) => {
+  return (
+    <section className={classes.detail}>
+      <img src={image} alt={title} className={classes.detail} />
+      <h1>{title}</h1>
+      <address>{address}</address>
+      <p>{description}</p>
+    </section>
+  );
+};
+export default MeetupDetail;
---
+++ nextjs-practice/pages/api/new-meetup.js
@@ -0,0 +1,26 @@
+import { MongoClient } from 'mongodb';
+
+// /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://ismail:ismail123@nextjspractice.afqrx.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupCollection = db.collection('meetups');
+
+    const result = await meetupCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
+}
+
+export default handler;
